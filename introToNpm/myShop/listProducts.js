677948c04f949a2737818f6f6285bbcb537eb5fc
bshridhar <shridhar.baddur@gmail.com>
2019-01-03 19:24:08 +0530

    npm exercise to list demo products completed

--- introToNpm/myShop/listProducts.js
+++ introToNpm/myShop/listProducts.js
@@ -2,6 +2,6 @@ console.log('=======================================================');
 console.log('Welcome to my shop');
 console.log('=======================================================');
 var product = require('faker');
-for(var i = 0; i < 10; i++) {
-    console.log(product.commerce.productName() + ' - ' + product.commerce.price());
+for(var i = 0; i < 100; i++) {
+    console.log(product.commerce.productName() + ' - $' + product.commerce.price());
 }
